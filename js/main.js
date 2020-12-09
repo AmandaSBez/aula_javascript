1b34b20c5ff58d453af156251dc9d8233e1d3d46
Amanda Bezerra <amanda.sousabez@gmail.com>
2020-12-09 17:07:23 -0200

    mais exemplos de js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var idade = 22;
+/*var idade = 22;
 var idade2 = 10;
 var nome = "Amanda Sousa";
 var frase="Japão é o melhor time do mundo";
@@ -7,3 +7,24 @@ var frase="Japão é o melhor time do mundo";
 console.log(nome);
 console.log(idade+idade2);
 console.log(frase.replace("Japão", "Brasil").toUpperCase());
+*/
+var lista = ["maçã", "pera", "laranja"];
+lista.push("uva");
+//lista.pop();
+//console.log(lista.reverse());
+//console.log(lista.length);
+
+//console.log(lista.toString()[0]);
+//console.log(lista.join(" - "));
+//console.log(lista);
+//alert(lista[1]);
+
+//Dicionario:
+//var fruta = {nome: "maçã", cor:"vermelha"};
+//console.log(fruta.nome);
+//alert(fruta.cor);
+
+//Lista de Dicionario:
+var frutas=[{ nome:"maçã",cor:"vermelha"},{nome:"uva",cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
