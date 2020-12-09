2f3d248bf8194d6c625b72fb4d98fc84d865db26
Amanda Bezerra <amanda.sousabez@gmail.com>
2020-12-09 16:37:27 -0200

    testando códigos js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-alert("meu primeiro js");
+var idade = 22;
+var idade2 = 10;
+var nome = "Amanda Sousa";
+var frase="Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.replace("Japão", "Brasil").toUpperCase());
